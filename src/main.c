e9d9d8cafa97f586316da316aea9a1a13a0ae67c
francis1009 <francism56789@gmail.com>
2025-07-20 08:27:12 +0800

    Add reading of assembly file

--- src/main.c
+++ src/main.c
@@ -6,5 +6,27 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	const char *asm_filename = argv[1];
+	printf("Attempting to read assembly file: %s\n", asm_filename);
+
+	char full_path[256];
+	int result = snprintf(full_path, sizeof(full_path), "asm/%s", filename);
+	if (result < 0 || result >= (int) sizeof(full_path)) {
+		fprintf(stderr, "Error: Assembly filename is too long.\n");
+		return 1;
+	}
+
+	FILE *file = fopen(full_path, "r");
+	if (file == NULL) {
+		perror("Error opening assembly file\n");
+		return 1;
+	}
+
+	char line[256];
+	while (fgets(line, sizeof(line), file)) {
+		printf("%s", line);
+	}
+
+	fclose(file);
     return 0;
 }
