99d9da3efd895971f9116aec1004ffcba68e0ff7
francis1009 <francism56789@gmail.com>
2025-07-21 18:34:12 +0800

    Fix wrong variable name in translator.h

--- include/translator.h
+++ include/translator.h
@@ -2,7 +2,7 @@
 #define TRANSLATOR_H
 
 const char *translator_comp_to_bin(char *comp);
-const char *translator_dest_to_bin(char *comp);
-const char *translator_jump_to_bin(char *comp);
+const char *translator_dest_to_bin(char *dest);
+const char *translator_jump_to_bin(char *jump);
 
 #endif
